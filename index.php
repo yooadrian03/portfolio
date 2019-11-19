60b89da29977894ef79d8175519784c36b770f44
yooma <adrianyoo2003@gmail.com>
2019-11-19 16:29:14 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Adrian Yoo & CSP:1st]</p>'; ?> 
+    <ul>
+     <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="index.html">Home</a></li>
+      <li><a href="./Yoo_AboutMe.html">About Me</a></li>
+      <li><a href="./Yoo_Projects.html">Projects</a></li>
+      <li><a href="./Yoo_Resume.html">Resume</a></li>
+      <li><a href="./Yoo_ContactMe.html">Contact Me</a></li>
+    </ul>
+  </body>
+</html>
